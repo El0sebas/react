1205ec2f6528b0231882f8a3b054b656e96d5fad
El0sebas <sebasmmadriga@gmail.com>
2026-02-23 20:52:02 -0500

    Actualizacion

--- src/features/layout/components/Props.jsx
+++ src/features/layout/components/Props.jsx
@@ -1,23 +1,32 @@
 import Avatar from "./Avatar"
-import hitler from "/react/images/hitler.jpg"
-import kim from "/react/images/kim.jpg"
-import putin from "/react/images/putin.jpg"
+
 const Props = () => {
+  const base = import.meta.env.BASE_URL
+
   return (
     <div className="container text-center mt-5">
       <h1 className="mb-4">Props React</h1>
 
       <div className="row">
         <div className="col">
-          <Avatar nombre="Adolf Hitler" imagen={hitler} />
+          <Avatar 
+            nombre="Adolf Hitler" 
+            imagen={`${base}images/hitler.jpg`} 
+          />
         </div>
 
         <div className="col">
-          <Avatar nombre="kim jong un" imagen={kim} />
+          <Avatar 
+            nombre="Kim Jong Un" 
+            imagen={`${base}images/kim.jpg`} 
+          />
         </div>
 
         <div className="col">
-          <Avatar nombre="Vladimir Putin" imagen={putin} />
+          <Avatar 
+            nombre="Vladimir Putin" 
+            imagen={`${base}images/putin.jpg`} 
+          />
         </div>
       </div>
     </div>
